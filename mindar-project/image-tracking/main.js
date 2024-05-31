d6a9cd6aa7b3a4a79591b65311770ee02116bfaf
IFormaWorld <hello@iforma.org>
2024-05-31 09:58:53 +0200

    Update

---
+++ mindar-project/image-tracking/main.js
@@ -0,0 +1,7 @@
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+    container: document.body
+  });
+});
