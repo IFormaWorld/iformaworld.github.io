9fa22adc153ccd7554ee43d0102822a661d6a3b8
IFormaWorld <hello@iforma.org>
2024-05-31 11:09:58 +0200

    Update

--- mindar-project/applications-20230306/applications/lectures/211-multi-detect/main.js
+++ mindar-project/applications-20230306/applications/lectures/211-multi-detect/main.js
@@ -5,26 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/musicband.mind'
+      imageTargetSrc: '../../assets/targets/razlaga-racuna.mind'
     });
     const {renderer, scene, camera} = mindarThree;
 
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const raccoon = await loadGLTF('../../assets/models/musicband-raccoon/scene.gltf');
+    const dobavitelj = await loadGLTF('../../assets/models/razlaga-racuna-dobavitelj/dobavitelj.gltf');
     raccoon.scene.scale.set(0.1, 0.1, 0.1);
     raccoon.scene.position.set(0, -0.4, 0);
 
-    const bear = await loadGLTF('../../assets/models/musicband-bear/scene.gltf');
+    const smm = await loadGLTF('../../assets/models/razlaga-racuna-smm/smm.gltf');
     bear.scene.scale.set(0.1, 0.1, 0.1);
     bear.scene.position.set(0, -0.4, 0);
 
     const raccoonAnchor = mindarThree.addAnchor(0);
-    raccoonAnchor.group.add(raccoon.scene);
+    raccoonAnchor.group.add(dobavitelj.scene);
 
     const bearAnchor = mindarThree.addAnchor(1);
-    bearAnchor.group.add(bear.scene);
+    bearAnchor.group.add(smm.scene);
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
